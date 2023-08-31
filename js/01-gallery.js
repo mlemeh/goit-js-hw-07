e093e5a0670f40290144cdae4d72053bbf5bd2c2
mlemeh <mlemeh@gmail.com>
2023-08-31 09:55:04 +0300

    c0831

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const str = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+   </li>`
+}).join(" ");
+
+gallery.insertAdjacentHTML("beforeend", str);
+
+gallery.addEventListener("click", handleOriginalView);
+
+function handleOriginalView(event) {
+    if (event.target === event.currentTarget) return;
+
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${event.target.dataset.source}" alt="${event.target.alt}">        
+        </div>
+    `)
+
+    instance.show();
+
+    const doc = document;
+    doc.addEventListener("keydown", exitESC);
+  
+    function exitESC(event) {
+      if (event.code === "Escape") {
+        doc.removeEventListener("keydown", exitESC);
+        instance.close();
+      }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const str = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+     </a>
+   </li>`
+}).join(" ");
+
+gallery.insertAdjacentHTML("beforeend", str);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+
